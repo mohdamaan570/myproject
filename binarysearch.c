e7c4db33b54fe83d6326d68a8500f5a7a42a222c
mohdamaan570 <mohdamaan570@gmail.com>
2021-11-10 23:13:27 +0530

    Modified1
    Signed-off-by: mohdamaan570 <mohdamaan570@gmail.com>

--- binarysearch.c
+++ binarysearch.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-    int a[100],i,n,search,lb,mid,ub;
+    int a[100],i,n,s,lb,mid,ub;
     printf("Enter the size of array\n");
     scanf("%d",&n);
     printf("Enter the elements of array\n");
@@ -9,18 +9,18 @@ void main()
         scanf("%d",&a[i]);
     }
     printf("Enter the element you want to search\n");
-    scanf("%d",&search);
+    scanf("%d",&s);
     lb=0;
     ub=n;
     mid=(lb+ub)/2;
 
     while (lb < ub)
     {
-       if(search < a[mid])
+       if(s < a[mid])
        ub = mid -1;
-       if(search > a[mid])
+       if(s > a[mid])
        lb = mid +1;
-       if(search == a[mid])
+       if(s == a[mid])
        printf("%d is found at %d",search,(mid+1));
 break;
     }
